916eaf26f79fae2597f4db100aa58c9295c76708
Saarthak Maini <saarthakmaini@gmail.com>
2025-08-12 10:38:32 +0530

    Change API url
    Signed-off-by: Saarthak Maini <saarthakmaini@gmail.com>

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = 'https://flavorforge-backend-3.onrender.com';
 
 // Helper function to handle API responses
 const handleResponse = async (response: Response) => {
